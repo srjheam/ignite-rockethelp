a95e780395092801765c33fc217a0e5513b161a5
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-07-17 22:44:32 -0300

    Add react-native-svg-transformer package

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,12 @@
 import { VStack, Heading } from 'native-base';
 
+import Logo from '../assets/logo_primary.svg'
+
 export function SignIn() {
   return (
     <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
+      <Logo />
+
       <Heading color='gray.100' fontSize='xl' mt={6} mb={20}>
         Olá, mundo!
       </Heading>
