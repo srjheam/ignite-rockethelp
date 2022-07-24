375cbff4567d6a28902aae5a0a85f7e7600e13b3
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-07-23 23:23:06 -0300

    Add Home screen, Filter component

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { THEME } from './src/styles/theme';
 
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,7 @@ export default function App() {
         translucent  
       />
 
-      { fontsLoaded ? <SignIn /> : <Loading /> }
+      { fontsLoaded ? <Home /> : <Loading /> }
     </NativeBaseProvider>
   )
 }
---
+++ src/components/Filter.tsx
@@ -0,0 +1,33 @@
+import { Button, IButtonProps, Text, useTheme } from "native-base";
+
+type Props = IButtonProps & {
+  title: string,
+  isActive?: boolean,
+  type: FilterType
+}
+
+export function Filter({ title, isActive = false, type, ...rest }: Props) {
+  const { colors } = useTheme();
+
+  const widthActive = isActive ? 1 : 0;
+  const colorType = type === 'open' ? colors.secondary[700] : colors.green[300];
+  const colorActive = isActive ? colorType : 'gray.300';
+
+  return (
+    <Button
+      variant='outline'
+      borderWidth={widthActive}
+      borderColor={colorType}
+      bg='gray.600'
+      flex={1}
+      size='sm'
+      {...rest}
+    >
+      <Text color={colorActive} fontSize='xs' textTransform='uppercase'>
+        {title}
+      </Text>
+    </Button>
+  );
+}
+
+export type FilterType = 'open' | 'closed';
