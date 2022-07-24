1f31a013bc051ff09e0424aa978d66e3ea231ce6
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-07-24 17:10:39 -0300

    Setup React Navigation

--- App.tsx
+++ App.tsx
@@ -3,11 +3,8 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import { Details } from './src/screens/Details';
-import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
-import { Register } from './src/screens/Register';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +17,7 @@ export default function App() {
         translucent  
       />
 
-      { fontsLoaded ? <Details /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </NativeBaseProvider>
   )
 }
