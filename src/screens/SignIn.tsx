033779ebc62df744dd613abae99309361f032e27
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-07-25 21:27:10 -0300

    Add authentication to Firebase

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -6,14 +7,39 @@ import Logo from '../assets/logo_primary.svg'
 
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const { colors } = useTheme();
 
-  function handleSignIn() { }
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert('Credenciais não informadas', 'Uma ou mais credenciais não foram informadas.');
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((e) => {
+        console.log(e);
+        setIsLoading(false);
+
+        if (e.code === 'auth/invalid-email') {
+          return Alert.alert('E-mail inválido', 'O e-mail informado é inválido.');
+        }
+
+        if (e.code === 'auth/wrong-password' || e.code === 'auth/user-not-found') {
+          return Alert.alert('Usuário incorreto', 'Uma ou mais crendenciais informadas estão incorretas');
+        }
+
+        return;
+      })
+  }
 
   return (
     <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
@@ -37,7 +63,7 @@ export function SignIn() {
         secureTextEntry
       />
 
-      <Button title='Entrar' w='full' onPress={handleSignIn} />
+      <Button title='Entrar' w='full' onPress={handleSignIn} isLoading={isLoading} />
     </VStack>
   )
 }
