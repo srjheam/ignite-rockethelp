ee71fabf0f2c7559425b445e204cc9b05aec4b9b
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-07-25 21:58:10 -0300

    Submit Register to Firestore

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
+import { Alert } from "react-native";
+
+import firestore from "@react-native-firebase/firestore";
+
+import { useNavigation } from "@react-navigation/native";
+
 import { VStack } from "native-base";
+
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navigation = useNavigation();
+
+  function handleSubmit() {
+    if (!patrimony || !description) {
+      return Alert.alert('Erro', 'Preencha todos os campos.');
+    }
+
+    setIsLoading(true);
+
+    firestore()
+      .collection('orders')
+      .add({
+        patrimony,
+        description,
+        status: 'open',
+        created_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() =>
+        Alert.alert('Cadastro', 'Solicitação cadastrada com sucesso.'))
+      .then(() =>
+        navigation.goBack())
+      .catch(e => {
+        setIsLoading(false);
+        Alert.alert('Erro', 'Não foi possível cadastrar a solicitação.');
+      })
+  }
+
   return (
     <VStack flex={1} p={6} bg='gray.600'>
       <Header title='Nova solicitação' />
@@ -11,19 +51,23 @@ export function Register() {
       <Input
         placeholder='Número do patrimônio'
         mt={4}
-      />
+        onChangeText={setPatrimony}
+        />
 
       <Input
         placeholder='Descrição do problema'
         flex={1}
         mt={5}
         textAlignVertical='top'
+        onChangeText={setDescription}
         multiline
       />
 
       <Button
         title='Cadastrar'
         mt={5}
+        isLoading={isLoading}
+        onPress={handleSubmit}
       />
     </VStack>
   );
